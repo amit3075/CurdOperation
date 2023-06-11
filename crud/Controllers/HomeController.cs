cbbd1db8e5e0cfde3fb76da956f0ebfcd00b4d59
amit3075 <amitpanthi3075@gmail.com>
2023-06-11 14:03:37 +0545

    curd added

---
+++ crud/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using crud.Models;
+using System.Diagnostics;
+
+namespace crud.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            student s1 = new student();
+            var l1 = s1.GetStudentList();
+            return View(l1);
+        }
+
+        public IActionResult edit(int id)
+        {
+            var student = new student();
+            var studentList = student.GetStudentList();
+            var model = studentList.Where(x => x.Id == id).FirstOrDefault();
+            if (model != null)
+                return View(model);
+            //foreach (var x in studentList)
+            //{
+            //    if (x.Id == id)
+            //    {
+            //        x.Name = model.Name;
+            //        x.Address = model.Address;
+            //        x.Age = model.Age;
+            //    }
+            //}
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ crud/Models/student.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace crud.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+
+
+        //[Required]
+        //[MinLength(5)]
+        public string Name { get; set; }
+
+        //[Required]
+        public string Address { get; set; }
+
+        //[Required]
+        //[EmailAddress]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public List<student> StudentList { get; set; }
+
+
+        public bool Checked { get; set; }
+        public int GetAge()
+        {
+            return 0;
+        }
+        //View Model:
+        //Domain model: Logic
+        //Entity
+
+
+        public List<student> GetStudentList()
+        {
+            var list = new List<student>();
+            var student1 = new student
+            {
+                Id = 1,
+                Name = "Gaurav",
+                Address = "KTM",
+                Age = 23
+            };
+            list.Add(student1);
+
+            var student2 = new student
+            {
+                Id = 2,
+                Name = "Sandip",
+                Address = "PKR",
+                Age = 22
+            };
+            list.Add(student2);
+            var student3 = new student
+            {
+                Id = 3,
+                Name = "Mandip",
+                Address = "BRT",
+                Age = 24
+            };
+            list.Add(student3);
+
+            return list;
+
+        }
+    }
+}
